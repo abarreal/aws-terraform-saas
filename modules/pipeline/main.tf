fa5e0f4b4c0ed35212e05e846e92e42a05d3e67b
AMB <39915766+abarreal@users.noreply.github.com>
2021-03-14 21:54:31 -0300

    Fix typo in comment

--- modules/pipeline/main.tf
+++ modules/pipeline/main.tf
@@ -25,7 +25,7 @@ resource "aws_s3_bucket" "artifact_store" {
   tags   = merge(local.artifact_store_tags, local.common_tags) 
 }
 
-# Define the pipeline to to log to.
+# Define the log group for the pipeline to log to.
 resource "aws_cloudwatch_log_group" "app_log_group" {
   name              = var.log_group_name
   retention_in_days = var.log_retention_in_days
